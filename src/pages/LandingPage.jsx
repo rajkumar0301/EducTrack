4c76d01adec151ae53681f5c7dd3e11ce875decf
RAJ KUMAR HEMBRAM <145889010+rajkumar0301@users.noreply.github.com>
2025-08-10 00:16:16 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./LandingPage.css";
+import "../styles/LandingPage.css";
 
 const LandingPage = () => {
   const [menuOpen, setMenuOpen] = useState(false);
